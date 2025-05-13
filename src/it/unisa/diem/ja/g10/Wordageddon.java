ccba45fdcc2f960e1bb3b705ac0f33075b1315a5
Davide Quaranta <d.quaranta8@studenti.unisa.it>
2025-05-13 15:22:05 +0200

    sos2

--- src/it/unisa/diem/ja/g10/Wordageddon.java
+++ src/it/unisa/diem/ja/g10/Wordageddon.java
@@ -14,7 +14,7 @@ public class Wordageddon extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/it/unisa/diem/ja/g10/assets/view/LoginScreen.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("/it/unisa/diem/ja/g10/assets/view/QuizScreen.fxml"));
         
         Scene scene = new Scene(root);
         
